18e9b42f58525870b7064be1be86346852f3cccf
Dipmalya <dipmalyasen@gmail.com>
2021-11-01 22:18:40 +0530

    React Calendar initial commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,11 +4,11 @@ import CustomCalendar from "./Calendar";
 export default function Main() {
   return (
     <div className="row m-4">
-      {['01', '02', '03', '04', '05', '06', '07', '08'].map((item, index) => (
+      {["01", "02", "03", "04", "05", "06", "07", "08", "09"].map((item, index) => (
         <CustomCalendar
-        key={`cal-${index}`}
-        classProp={"pr-1 pb-1"} 
-        value={new Date(`${item}/01/2021`)}
+          key={`cal-${index}`}
+          classProp={"pr-1 pb-1"}
+          value={new Date(`${item}/01/2021`)}
         />
       ))}
     </div>
