ca529dae69156a823e8f95853667a00fdc8d7ab9
Dipmalya <dipmalyasen@gmail.com>
2021-11-07 15:54:44 +0530

    Updated calendar set with manual implementation

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,36 +1,41 @@
 import React, { Component } from "react";
 import "./index.css";
 import config from "../assets/config.json";
+import { DayIndex } from "./dayconfig";
 
 export default class CustomCalendar extends Component {
-  getDate(dayCount) {
+  shoudShowDate(dayCount, index) {
     const { monthIndex } = this.props;
-      if (dayCount <= config[monthIndex].days ) {
-          return dayCount
-      }
-      else {
-          return ''
-      }
+    const firstDate = `${monthIndex}/01/${new Date().getFullYear()}`;
+    const startDay = new Date(firstDate).toDateString().slice(0, 3);
+    if (dayCount > 1 || DayIndex[index] === startDay) {
+      return dayCount <= config[monthIndex].days;
+    } else {
+      return false;
+    }
   }
   render() {
     const { monthIndex } = this.props;
+    let dayCount = 1;
     return (
       <div className="w-100 col-md-4 pr-1 pb-1 pl-0">
         <div className="month-class">
           {config[monthIndex] ? config[monthIndex].name : ""}
         </div>
         <div className="calendar-date">
-          {Array.from({ length: 5 }, (v, i) => i).map((day) => {
+          {Array.from({ length: 6 }, (v, i) => i).map((day) => {
             return (
               <div className="calendar-week">
                 {Array.from({ length: 7 }, (v, i) => i + 1).map((day1) => (
                   <>
                     {day1 > 5 ? (
                       <div className="day weekend-day">
-                        {this.getDate(day * 7 + day1)}
+                        {this.shoudShowDate(dayCount, day1) ? dayCount++ : ""}
                       </div>
                     ) : (
-                      <div className="day">{this.getDate(day * 7 + day1)}</div>
+                      <div className="day">
+                        {this.shoudShowDate(dayCount, day1) ? dayCount++ : ""}
+                      </div>
                     )}
                   </>
                 ))}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import CustomCalendar from "./Calendar";
+import { DayIndex } from "./dayconfig";
 
 export default function Main() {
   return (
     <div className="row m-4">
       <div className="col-md-9">
+        <div className="row">
+          {Array.from({ length: 3 }, (v, i) => i + 1).map((week) => (
+            <div className="d-flex mr-1 week-day">
+              {Object.values(DayIndex).map((item, index) =>
+                index < 5 ? (
+                  <div className="mr-3" style={{paddingLeft: '0.2rem'}}>{item}</div>
+                ) : (
+                  <div className="weekends pl-2">{item}</div>
+                )
+              )}
+            </div>
+          ))}
+        </div>
         <div className="row">
           {["1", "2", "3", "4", "5", "6", "7", "8", "9"].map((item, index) => (
             <CustomCalendar monthIndex={item} />
